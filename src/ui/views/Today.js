25bb2b3f3ece5f1b290ecda7b5c5832567f47801
lukehaas <lukehaas@users.noreply.github.com>
2018-04-30 18:58:39 +0100

    add update time

--- src/ui/views/Today.js
+++ src/ui/views/Today.js
@@ -5,6 +5,21 @@ module.exports = class Today {
     Object.assign(this, options)
   }
 
+  _updated() {
+    const x = this.width - 115
+    const y = this.y + 9
+    const time = moment().format('HH:mm')
+
+    const updateText = {
+      x,
+      y,
+      baseline: 'top',
+      value: `Updated at ${time}`,
+      style: { font: `11px "${this.font}"`, fill: this.fg }
+    }
+    this.sctx.text(updateText)
+  }
+
   _date() {
     const x = this.x + 24
     const y = this.y + 18
@@ -19,10 +34,11 @@ module.exports = class Today {
     }
     this.sctx.text(dayText)
 
-    return y + 52
+    return y + 48
   }
 
   async draw() {
+    this._updated()
     const height = this._date()
 
     return { height }
