0b45ae7cae5db18d04f888732879d0306dad2679
Luke Haas <lukehaas7@gmail.com>
2019-06-13 21:58:17 +0100

    upgrade to v2 of OD api

--- src/data-sources/Word.js
+++ src/data-sources/Word.js
@@ -1,8 +1,48 @@
 const Twit = require('twit')
-const Dictionary = require('oxford-dictionary-api')
+const https = require('https')
 const { path, compose } = require('ramda')
 const { wordModel } = require('../models/model')
 const config = require('../config')
+
+class Dictionary {
+  constructor(appId, appKey) {
+    this.appId = appId
+    this.appKey = appKey
+  }
+
+  find(word, cb) {
+    const options = {
+      host : 'od-api.oxforddictionaries.com',
+      port : 443,
+      path : `/api/v2/entries/en-gb/${word}`,
+      method : 'GET',
+      headers : {
+        'Accept': 'application/json',
+        'app_id': this.appId,
+        'app_key': this.appKey,
+      }
+    }
+    https.get(options, res => {
+      if(res.statusCode == 404){
+        cb('No such entry found')
+      } else {
+        let data = ''
+        res.on('data', chunk => {
+            data += chunk
+        }).on('end', () => {
+          let result = {}
+          try {
+            result = JSON.parse(data)
+          } catch (exp) {
+            result = {'status_code': 500, 'status_text': 'JSON Parse Failed'}
+          }
+          cb(null,result)
+        }).on('error', err => {
+          cb(err)
+        })}
+    })
+  }
+}
 
 module.exports = class Word {
   constructor() {
